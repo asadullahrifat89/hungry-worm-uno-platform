afa03cb1ca0efdd0f81659dcc3cb03b17da8458f
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-17 11:52:21 +0600

    basic game page added

--- SnakeGame/SnakeGame.Shared/Pages/GamePage.xaml.cs
+++ SnakeGame/SnakeGame.Shared/Pages/GamePage.xaml.cs
@@ -1,30 +1,352 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.System;
 
 namespace SnakeGame
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class GamePage : Page
     {
+        #region Fields
+        
+        private PeriodicTimer _gameViewTimer;
+        private readonly TimeSpan _frameTime = TimeSpan.FromMilliseconds(Constants.DEFAULT_FRAME_TIME);
+
+        private readonly Random _random = new();
+        private Rect _playerHitBox;
+
+        private int _gameSpeed;
+        private readonly int _defaultGameSpeed = 5;
+
+        private bool _moveLeft;
+        private bool _moveRight;
+        private bool _moveUp;
+        private bool _moveDown;
+        private bool _isGameOver;
+        private bool _isPointerActivated;
+
+
+        private double _windowHeight, _windowWidth;
+        private double _scale;
+        private Point _pointerPosition;
+
+        private double _score;
+
+        #endregion
+
+        #region Ctor
+
         public GamePage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            _isGameOver = true;
+            ShowInGameTextMessage("TAP_ON_SCREEN_TO_BEGIN");
+
+            _windowHeight = Window.Current.Bounds.Height;
+            _windowWidth = Window.Current.Bounds.Width;
+
+            Loaded += GamePage_Loaded;
+            Unloaded += GamePage_Unloaded;
         }
+
+        #endregion
+
+        #region Events
+
+        #region Page
+
+        private void GamePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            SizeChanged += GamePage_SizeChanged;
+        }
+
+        private void GamePage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SizeChanged -= GamePage_SizeChanged;
+            StopGame();
+        }
+
+        private void GamePage_SizeChanged(object sender, SizeChangedEventArgs args)
+        {
+            _windowWidth = args.NewSize.Width;
+            _windowHeight = args.NewSize.Height;
+
+            SetViewSize();
+
+            Console.WriteLine($"WINDOWS SIZE: {_windowWidth}x{_windowHeight}");
+        }
+
+        #endregion
+
+        #region Input
+
+        private void InputView_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_isGameOver)
+            {
+                InputView.Focus(FocusState.Programmatic);
+                StartGame();
+            }
+            else
+            {
+                _isPointerActivated = true;
+            }
+        }
+
+        private void InputView_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (_isPointerActivated)
+            {
+                PointerPoint point = e.GetCurrentPoint(GameView);
+                _pointerPosition = point.Position;
+            }
+        }
+
+        private void InputView_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            _isPointerActivated = false;
+            _pointerPosition = null;
+        }
+
+        private void OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Left)
+            {
+                _moveLeft = true;
+                _moveRight = false;
+            }
+            if (e.Key == VirtualKey.Right)
+            {
+                _moveRight = true;
+                _moveLeft = false;
+            }
+            if (e.Key == VirtualKey.Up)
+            {
+                _moveUp = true;
+                _moveDown = false;
+            }
+            if (e.Key == VirtualKey.Down)
+            {
+                _moveDown = true;
+                _moveUp = false;
+            }
+        }
+
+        private void OnKeyUP(object sender, KeyRoutedEventArgs e)
+        {
+            // when the player releases the left or right key it will set the designated boolean to false
+            if (e.Key == VirtualKey.Left)
+            {
+                _moveLeft = false;
+            }
+            if (e.Key == VirtualKey.Right)
+            {
+                _moveRight = false;
+            }
+            if (e.Key == VirtualKey.Up)
+            {
+                _moveUp = false;
+            }
+            if (e.Key == VirtualKey.Down)
+            {
+                _moveDown = false;
+            }
+        }
+
+        #endregion
+
+        #region Button
+
+        private void QuitGameButton_Checked(object sender, RoutedEventArgs e)
+        {
+            PauseGame();
+        }
+
+        private void QuitGameButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ResumeGame();
+        }
+
+        private void ConfirmQuitGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigateToPage(typeof(StartPage));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Animation
+
+        #region Game
+
+        private void StartGame()
+        {
+#if DEBUG
+            Console.WriteLine("GAME STARTED");
+#endif
+            HideInGameTextMessage();
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+          
+
+            _gameSpeed = _defaultGameSpeed;
+          
+
+            ResetControls();
+
+            _isGameOver = false;
+
+            _score = 0;         
+            scoreText.Text = "0";
+
+            foreach (GameObject x in GameView.Children.OfType<GameObject>())
+            {
+                GameView.AddDestroyableGameObject(x);
+            }
+            
+            RemoveGameObjects();
+
+            StartGameSounds();
+            RunGame();
+        }
+
+        private void RemoveGameObjects()
+        {
+            GameView.RemoveDestroyableGameObjects();
+        }
+
+        private async void RunGame()
+        {
+            _gameViewTimer = new PeriodicTimer(_frameTime);
+
+            while (await _gameViewTimer.WaitForNextTickAsync())
+            {
+                GameViewLoop();
+            }
+        }
+
+        private void GameViewLoop() 
+        {
+        
+        }
+
+        private void PauseGame()
+        {
+            InputView.Focus(FocusState.Programmatic);
+            ShowInGameTextMessage("GAME_PAUSED");
+
+            _gameViewTimer?.Dispose();
+
+            ResetControls();
+
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+            PauseGameSounds();
+        }
+
+        private void ResumeGame()
+        {
+            InputView.Focus(FocusState.Programmatic);
+            HideInGameTextMessage();
+
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+            SoundHelper.ResumeSound(SoundType.BACKGROUND);
+            SoundHelper.ResumeSound(SoundType.CAR_ENGINE);
+
+            RunGame();
+        }
+
+        private void StopGame()
+        {
+            _gameViewTimer?.Dispose();
+            StopGameSounds();
+        }
+
+        private void ResetControls()
+        {
+            _moveLeft = false;
+            _moveRight = false;
+            _moveUp = false;
+            _moveDown = false;
+            _isPointerActivated = false;
+        }
+
+        #endregion 
+
+        #endregion
+
+        #region Sound
+
+        private async void StartGameSounds()
+        {
+            SoundHelper.PlaySound(SoundType.CAR_START);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            SoundHelper.PlaySound(SoundType.CAR_ENGINE);
+
+            SoundHelper.RandomizeBackgroundSound();
+            SoundHelper.PlaySound(SoundType.BACKGROUND);
+        }
+
+        private void StopGameSounds()
+        {
+            SoundHelper.StopSound(SoundType.BACKGROUND);
+            SoundHelper.StopSound(SoundType.CAR_ENGINE);
+        }
+
+        private void PauseGameSounds()
+        {
+            SoundHelper.PauseSound(SoundType.BACKGROUND);
+            SoundHelper.PauseSound(SoundType.CAR_ENGINE);
+        }
+
+        #endregion
+
+        #region Page
+
+        private void SetViewSize()
+        {
+            _scale = ScalingHelper.GetGameObjectScale(_windowWidth);          
+
+            GameView.Width = _windowWidth;
+            GameView.Height = _windowHeight;
+        }
+
+        private void NavigateToPage(Type pageType)
+        {
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+            App.NavigateToPage(pageType);
+        }
+
+        #endregion
+
+        #region In Game Message
+
+        private void ShowInGameTextMessage(string resourceKey)
+        {
+            InGameMessageText.Text = LocalizationHelper.GetLocalizedResource(resourceKey);
+            InGameMessagePanel.Visibility = Visibility.Visible;
+        }
+
+        private void HideInGameTextMessage()
+        {
+            InGameMessageText.Text = "";
+            InGameMessagePanel.Visibility = Visibility.Collapsed;
+        }
+
+        #endregion
+
+        #endregion
     }
 }
